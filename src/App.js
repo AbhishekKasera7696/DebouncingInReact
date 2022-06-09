2e8c84310b495396df0028c7f8f686cb569c8e8b
AbhishekKasera7696 <akasera62@gmail.com>
2022-06-09 17:19:29 +0530

    debouncigReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useEffect, useState } from 'react';
+import useDebounce from './useDebounce';
 
 function App() {
+  const [searchTerm,setSearchTerm] = useState('');
+  const [data,setData] = useState([])
+  const debounceSearchTerm = useDebounce(searchTerm,300);
+
+  useEffect(()=> {
+    if (debounceSearchTerm){
+      console.log('Search Term :' , debounceSearchTerm)
+      fetchData(debounceSearchTerm)
+    }else{
+      console.log('Something else')
+    }
+  },[debounceSearchTerm])
+
+
+  const fetchData = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then((res) => res.json())
+    .then((response) => {
+      console.log('Response', response)
+      setData(response)
+    })
+  }
+
+  const search = (data,key) => {
+     for(let i=0; i<data.length; i++){
+         if(data[i].name===key){
+           return true;
+         }
+      
+     }
+     return false;
+  }
+
+
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+       placeholder='write here..'></input>
+       {
+         search(data,searchTerm)?<h1>{searchTerm}</h1>:
+         <h1>NO Data</h1>
+       }
     </div>
   );
 }
